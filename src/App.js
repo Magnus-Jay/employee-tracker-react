3f4123a353139f73d72f238cd2a1180f4a926cfc
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-06 21:50:19 -0800

    file name update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import API from './utils/API'
+import API from './utils/api'
 import React from "react";
 import EmployeeTableBuilder from './components/EmployeeTableBuilder';
 
